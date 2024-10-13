7cad142e64e57fb23af7fa40d9e70b99569ddd6c
orange-na <otuy1125abman@gmail.com>
2024-10-13 12:17:17 +0900

    人口構成APIレスポンスから、X軸:年、Y軸:人口数の折れ線グラフを動的に生成して表示する

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,63 +1,15 @@
-"use client";
-
-import { useEffect, useState } from "react";
+import PrefectureCheckbox from "@/features/prefectureCheckbox";
 import styles from "./page.module.css";
-import { Prefecture } from "@/types";
-import { env } from "@/env/env";
+import PopulationChart from "@/features/populationChart";
+import { fetchPrefectures } from "@/libs";
 
-export default function Home() {
-  const [prefectures, setPrefectures] = useState<Prefecture[]>([]);
-
-  const fetchPrefectures = async () => {
-    const response = await fetch(
-      "https://opendata.resas-portal.go.jp/api/v1/prefectures",
-      {
-        headers: {
-          "X-API-KEY": env.NEXT_PUBLIC_RESAS_API_KEY,
-        },
-      }
-    );
-    const data = await response.json();
-    setPrefectures(data.result);
-  };
-
-  const fetchPopulation = async (prefCode: number) => {
-    const response = await fetch(
-      `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=${prefCode}`,
-      {
-        headers: {
-          "X-API-KEY": env.NEXT_PUBLIC_RESAS_API_KEY,
-        },
-      }
-    );
-    const data = await response.json();
-    console.log(data.result.data[0].data);
-  };
-
-  useEffect(() => {
-    fetchPrefectures();
-  }, []);
+export default async function Home() {
+  const prefectures = await fetchPrefectures();
 
   return (
     <div className={styles.container}>
-      {prefectures.map((prefecture) => (
-        <div key={prefecture.prefCode}>
-          <input
-            type="checkbox"
-            name="prefecture"
-            value={prefecture.prefCode}
-            id={`prefecture-${prefecture.prefCode}`}
-            className={styles.checkbox}
-            onChange={() => fetchPopulation(prefecture.prefCode)}
-          />
-          <label
-            htmlFor={`prefecture-${prefecture.prefCode}`}
-            className={styles.checkbox_label}
-          >
-            {prefecture.prefName}
-          </label>
-        </div>
-      ))}
+      <PrefectureCheckbox prefectures={prefectures} />
+      <PopulationChart prefectures={prefectures} />
     </div>
   );
 }
