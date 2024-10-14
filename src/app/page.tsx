671533eaf99bbb3d3fab80400329af8707bbe244
orange-na <otuy1125abman@gmail.com>
2024-10-14 10:12:03 +0900

    修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,9 @@ import { fetchPrefectures } from "@/libs";
 
 export default async function Home() {
   const prefectures = await fetchPrefectures();
+  if (!prefectures) {
+    return <div>Failed to fetch prefectures</div>;
+  }
 
   return (
     <div className={styles.container}>
