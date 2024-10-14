fb6b893adeb9062207c7d8c1a05ad2c64ebd61b4
orange-na <otuy1125abman@gmail.com>
2024-10-14 10:38:19 +0900

    not-foundページの追加

---
+++ src/app/not-found.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import styles from "./not-found.module.css";
+
+export default function NotFound() {
+  return (
+    <main className={styles.container}>
+      <h2 className={styles.heading}>404 Not Found</h2>
+      <p>
+        お探しのページが見つかりませんでした。URLをご確認の上、再度アクセスしてください。
+      </p>
+      <Link href="/" className={styles.link}>
+        戻る
+      </Link>
+    </main>
+  );
+}
