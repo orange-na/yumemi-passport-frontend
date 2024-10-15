0491a3826542980f4ca3697db73c6a40041775a3
orange-na <otuy1125abman@gmail.com>
2024-10-15 21:21:57 +0900

    footer追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layouts/header";
+import Footer from "@/components/layouts/footer";
 
 const notoSansJp = Noto_Sans_JP({
   subsets: ["latin"],
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className={notoSansJp.className}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
