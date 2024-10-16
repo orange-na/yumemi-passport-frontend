48f3fbd6ea10af97c4f7b02531a3c395233bd1f8
orange-na <otuy1125abman@gmail.com>
2024-10-16 19:58:09 +0900

    修正

--- src/e2e/index.spec.ts
+++ src/e2e/index.spec.ts
@@ -18,17 +18,17 @@ test("都道府県の人口データを表示する", async ({ page }) => {
   await expect(page.getByText("人口数(万人)")).toBeVisible();
 });
 
-// test("選択した都道府県のチェックを外す", async ({ page }) => {
-//   await page.getByLabel("北海道").check();
-//   await page.getByLabel("青森県").check();
+test("選択した都道府県のチェックを外す", async ({ page }) => {
+  await page.getByLabel("北海道").check();
+  await page.getByLabel("青森県").check();
 
-//   await page.getByLabel("北海道").uncheck();
+  await page.getByLabel("北海道").uncheck();
 
-//   await expect(page.getByRole("list").getByText("北海道")).not.toBeVisible();
-//   await expect(page.getByRole("list").getByText("青森県")).toBeVisible();
-//   await expect(page.getByText("年度(年)")).toBeVisible();
-//   await expect(page.getByText("人口数(万人)")).toBeVisible();
-// });
+  await expect(page.getByRole("list").getByText("北海道")).not.toBeVisible();
+  await expect(page.getByRole("list").getByText("青森県")).toBeVisible();
+  await expect(page.getByText("年度(年)")).toBeVisible();
+  await expect(page.getByText("人口数(万人)")).toBeVisible();
+});
 
 test("存在しないページへのアクセス", async ({ page }) => {
   await page.goto("http://localhost:3000/invalid");
