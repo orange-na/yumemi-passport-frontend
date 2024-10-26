5fd7dfc6c8b1649c9896b6f5f61d03f95cd563c2
orange-na <otuy1125abman@gmail.com>
2024-10-26 12:35:38 +0900

    コードにマジックナンバーが存在している

--- src/features/populationChart/index.tsx
+++ src/features/populationChart/index.tsx
@@ -29,77 +29,75 @@ export default function PopulationChart({ prefectures }: Props) {
   } = usePopulationData(selectedPrefectures, prefectures);
 
   return (
-    <>
-      <div className={styles.lineChartContainer}>
-        <div className={styles.populationLabels}>
-          {populationLabels.map((populationLabel) => (
-            <button
-              type="button"
-              key={populationLabel}
-              className={`${styles.populationLabel} ${
-                selectedPopulationLabel === populationLabel ? styles.active : ""
-              }`}
-              onClick={() => setSelectedPopulationLabel(populationLabel)}
-            >
-              {populationLabel}
-            </button>
-          ))}
-        </div>
-        <ResponsiveContainer width="100%" height={500}>
-          <LineChart data={populationData} className={styles.lineChart}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis
-              dataKey="year"
-              height={50}
-              padding={{ left: 20, right: 20 }}
-              label={{
-                value: "年度(年)",
-                position: "insideBottom",
-                offset: 0,
-              }}
-            />
-            <YAxis
-              tickFormatter={(value) => `${value / 10000}`}
-              label={{
-                value: "人口数(万人)",
-                angle: -90,
-                position: "insideLeft",
-              }}
-            />
-            <Tooltip
-              contentStyle={{ backgroundColor: "#ffffffcc" }}
-              wrapperStyle={{
-                maxHeight: 200,
-                overflowY: "auto",
-              }}
-            />
-            <Legend
-              verticalAlign="bottom"
-              height={60}
-              wrapperStyle={{
-                overflowY: "auto",
-              }}
-            />
-            {Object.keys(populationData[0] || {})
-              .filter((key) => key !== "year")
-              .map((prefName, index) => {
-                const hue = (index * 137.5) % 360;
-                const color = `hsl(${hue}, 100%, 65%)`;
-                return (
-                  <Line
-                    key={prefName}
-                    type="monotone"
-                    dataKey={prefName}
-                    stroke={color}
-                    strokeWidth={2}
-                    dot={{ r: 2 }}
-                    activeDot={{ r: 8 }}
-                  />
-                );
-              })}
-          </LineChart>
-        </ResponsiveContainer>
+    <div className={styles.lineChartContainer}>
+      <div className={styles.populationLabels}>
+        {populationLabels.map((populationLabel) => (
+          <button
+            type="button"
+            key={populationLabel}
+            className={`${styles.populationLabel} ${
+              selectedPopulationLabel === populationLabel ? styles.active : ""
+            }`}
+            onClick={() => setSelectedPopulationLabel(populationLabel)}
+          >
+            {populationLabel}
+          </button>
+        ))}
       </div>
-    </>
+      <ResponsiveContainer width="100%" height={500}>
+        <LineChart data={populationData} className={styles.lineChart}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis
+            dataKey="year"
+            height={50}
+            padding={{ left: 20, right: 20 }}
+            label={{
+              value: "年度(年)",
+              position: "insideBottom",
+              offset: 0,
+            }}
+          />
+          <YAxis
+            tickFormatter={(value) => `${value / 10000}`}
+            label={{
+              value: "人口数(万人)",
+              angle: -90,
+              position: "insideLeft",
+            }}
+          />
+          <Tooltip
+            contentStyle={{ backgroundColor: "#ffffffcc" }}
+            wrapperStyle={{
+              maxHeight: 200,
+              overflowY: "auto",
+            }}
+          />
+          <Legend
+            verticalAlign="bottom"
+            height={60}
+            wrapperStyle={{
+              overflowY: "auto",
+            }}
+          />
+          {Object.keys(populationData[0] || {})
+            .filter((key) => key !== "year")
+            .map((prefName, index) => {
+              const hue = (index * 137.5) % 360;
+              const color = `hsl(${hue}, 100%, 65%)`;
+              return (
+                <Line
+                  key={prefName}
+                  type="monotone"
+                  dataKey={prefName}
+                  stroke={color}
+                  strokeWidth={2}
+                  dot={{ r: 2 }}
+                  activeDot={{ r: 8 }}
+                />
+              );
+            })}
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
   );
 }
